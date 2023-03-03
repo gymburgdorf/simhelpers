6ac3287c8c16c775efec1d70605b82a62e23c519
gymburgdorf <79353439+gymburgdorf@users.noreply.github.com>
2023-03-03 10:37:50 +0000

    Deploying to gh-pages from @ gymburgdorf/simhelpers@4bcd1617b5eb636e4406daf422bcd81eb4a6bcc8 🚀

--- simhelpers.js
+++ simhelpers.js
@@ -1,5 +1,6 @@
-import * as PIXI from 'pixi.js';
-class World {
+// @ts-ignore Import module
+import * as PIXI from 'https://cdnjs.cloudflare.com/ajax/libs/pixi.js/7.1.4/pixi.min.mjs';
+export class World {
     constructor() {
         this.app = new PIXI.Application();
     }
